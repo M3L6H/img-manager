cf25912b166a02356ca74a171a76fda87f447917
Michael Hollingworth <sisoac@hotmail.com>
2022-08-30 17:09:54 -0500

    Add Tag & ImageTag models

--- src/lib/models.py
+++ src/lib/models.py
@@ -12,7 +12,19 @@ class Image:
       self.last_seen = datetime.now()
 
   def __repr__(self):
-    return f"<local_path: '{self.local_path}'; thumbnail_path: '{self.thumbnail_path}'; cloud_url: '{self.cloud_url}'>"
+    return f"<local_path: '{self.local_path}'; thumbnail_path: '{self.thumbnail_path}'; cloud_url: '{self.cloud_url}'; last_seen: '{self.last_seen}'>"
 
   def __str__(self):
-    return f"local_path: '{self.local_path}'; thumbnail_path: '{self.thumbnail_path}'; cloud_url: '{self.cloud_url}'"
+    return f"local_path: '{self.local_path}'; thumbnail_path: '{self.thumbnail_path}'; cloud_url: '{self.cloud_url}'; last_seen: '{self.last_seen}'"
+
+@db.model
+class ImageTag:
+  def __init__(self, image: db.ForeignKey[Image], tag: db.ForeignKey["Tag"]):
+    self.image = image
+    self.tag = tag
+
+@db.model
+class Tag:
+  def __init__(self, name: str, parent: db.ForeignKey["Tag"]=None):
+    self.name = name
+    self.parent = parent
