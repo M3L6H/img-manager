fecb633ed64dda46adf723b1fa26171ee30a2b4a
Michael Hollingworth <sisoac@hotmail.com>
2022-08-30 07:36:50 -0500

    Add timestamp

--- src/lib/models.py
+++ src/lib/models.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 import db
 
 @db.model
 class Image:
-  def __init__(self, local_path: str = "", thumbnail_path: str = "", cloud_url: str = ""):
+  def __init__(self, local_path: str="", thumbnail_path: str="", cloud_url: str="", last_seen: datetime=None):
     self.local_path = local_path
     self.thumbnail_path = thumbnail_path
     self.cloud_url = cloud_url
+
+    if last_seen is None:
+      self.last_seen = datetime.now()
 
   def __repr__(self):
     return f"<local_path: '{self.local_path}'; thumbnail_path: '{self.thumbnail_path}'; cloud_url: '{self.cloud_url}'>"
