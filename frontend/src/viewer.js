bdf029416b9aad2e92e8228d8a9da8df9038fe8a
Michael Hollingworth <sisoac@hotmail.com>
2022-11-08 15:55:19 -0600

    Draw rectangles

--- frontend/src/viewer.js
+++ frontend/src/viewer.js
@@ -1,6 +1,25 @@
 import VideoPlayer from "./videoPlayer.js";
 import * as utils from "./utils.js";
 
+const COLORS = [
+  "185, 219, 63",
+  "221, 55, 83",
+  "127, 67, 183",
+  "51, 94, 224",
+  "244, 206, 17",
+  "99, 252, 255",
+  "70, 233, 242",
+  "7, 130, 62",
+  "242, 212, 19",
+  "223, 50, 229",
+  "47, 183, 65",
+  "216, 121, 73",
+  "45, 49, 173",
+  "49, 206, 196",
+  "194, 201, 2",
+  "81, 194, 198"
+];
+
 /**
  * Viewer class containing all the functionality to manage the viewport
  */
@@ -10,12 +29,14 @@ export default class Viewer {
   static ZOOM_INCREMENT = 0.04;
 
   constructor() {
+    this._canvas = document.getElementById("canvas");
     this._image = document.getElementById("image");
     this._media = document.getElementById("media");
     this._video = document.getElementById("video");
     this._viewer = document.getElementById("viewer");
 
     this.videoPlayer = new VideoPlayer(this);
+    this.ctx = this._canvas.getContext("2d");
 
     this._mediaHeight = 0;
     this._mediaWidth = 0;
@@ -26,17 +47,25 @@ export default class Viewer {
     this._scale = 1;
     this._minScale = 1;
 
+    // Data
+    this._data = {};
+
     // State
+    this._drawingRect = false;
     this._panning = false;
 
     // Register listeners
     window.addEventListener("resize", () => this.resizeHandler());
+    this._canvas.addEventListener("click", (e) => this.canvasClickHandler(e));
     this._video.addEventListener("loadedmetadata", (e) => this.videoLoadHandler(e), false);
     this._viewer.addEventListener("mousemove", (e) => this.mouseMoveHandler(e));
     this._viewer.addEventListener("wheel", (e) => this.zoom(e));
 
     // Initial load
     this.cacheViewerDimensions();
+    this.resizeCanvas();
+
+    requestAnimationFrame(() => this.drawFrame());
   }
 
   /*****************************************************************************
@@ -44,11 +73,50 @@ export default class Viewer {
   *****************************************************************************/
 
   /**
+   * Private helper to check if a coordinate pair is within media limits
+   * @param {Number} x X position to check
+   * @param {Number} y Y position to check
+   * @returns Whether x and y are in the current media limits
+   */
+  _inLimits(x, y) {
+    const { topLimit, bottomLimit, leftLimit, rightLimit } = this.limits;
+    return leftLimit < x && x < rightLimit && topLimit < y && y < bottomLimit;
+  }
+
+  /**
    * Called to update the cached dimensions of the viewer
    */
   cacheViewerDimensions() {
     this._viewerHeight = this._viewer.offsetHeight;
     this._viewerWidth = this._viewer.offsetWidth;
+  }
+
+  /**
+   * Called to draw a frame on the canvas
+   */
+  drawFrame() {
+    this.ctx.clearRect(0, 0, this._viewerWidth, this._viewerHeight);
+
+    for (let i = 0; i < this._rects.length; ++i) {
+      const { x1, y1, x2, y2, hovered } = this._rects[i];
+      const colorIdx = parseInt(`${(x1 % 4).toString(2)}${(y1 % 4).toString(2)}`, 2);
+      let strokeAlpha = 0.5, fillAlpha = 0.25;
+
+      if (this._drawingRect && i == this._rects.length - 1) {
+        strokeAlpha = 1.0;
+        fillAlpha = 0;
+      } else if (hovered) {
+        strokeAlpha = 1.0;
+        fillAlpha = 0.75;
+      }
+
+      this.ctx.strokeStyle = `rgba(${COLORS[colorIdx]}, ${strokeAlpha})`;
+      this.ctx.fillStyle = `rgba(${COLORS[colorIdx]}, ${fillAlpha})`;
+      this.ctx.fillRect(x1, y1, x2 - x1, y2 - y1);
+      this.ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+    }
+
+    requestAnimationFrame(() => this.drawFrame());
   }
 
   /**
@@ -116,6 +184,14 @@ export default class Viewer {
 
     this.mediaDimensions = [height, width];
     this.mediaPosition = [top, left];
+  }
+
+  /**
+   * Called after a window resize to adjust the canvas
+   */
+  resizeCanvas() {
+    this._canvas.height = this._viewerHeight;
+    this._canvas.width = this._viewerWidth;
   }
 
   /**
@@ -190,6 +266,23 @@ export default class Viewer {
   *****************************************************************************/
 
   /**
+   * Called to handle clicks on a canvas
+   */
+   canvasClickHandler(e) {
+    if (this._drawingRect) {
+      this._drawingRect = false;
+    } else if (this._inLimits(e.clientX, e.clientY)) {
+      this._drawingRect = true;
+      this._rects.push({
+        x1: e.clientX,
+        y1: e.clientY,
+        x2: e.clientX,
+        y2: e.clientY
+      });
+    }
+  }
+
+  /**
    * Called when an image is loaded
    * @param {HTMLImageElement} img The image element used to load the image
    */
@@ -204,21 +297,18 @@ export default class Viewer {
   }
 
   /**
-   * Called when a video is loaded
-   */
-  videoLoadHandler() {
-    this._mediaHeight = this._video.videoHeight;
-    this._mediaWidth = this._video.videoWidth;
-    this._aspectRatio = this._mediaWidth / this._mediaHeight;
-    this.resetMedia();
-  }
-
-  /**
    * Called when the mouse moves
    * @param {MouseEvent} e Event fired when the mouse moves
    */
   mouseMoveHandler(e) {
-    if (this._panning) {
+    if (this._drawingRect && this._rects.length > 0) {
+      const lastIdx = this._rects.length - 1;
+
+      const { topLimit, bottomLimit, leftLimit, rightLimit } = this.limits;
+
+      this._rects[lastIdx].x2 = Math.max(Math.min(e.clientX, rightLimit), leftLimit);
+      this._rects[lastIdx].y2 = Math.max(Math.min(e.clientY, bottomLimit), topLimit);
+    } else if (this._panning) {
       this.mediaPosition = [
         this._oldMediaTop + e.clientY - this._oldMouseY,
         this._oldMediaLeft + e.clientX - this._oldMouseX
@@ -228,6 +318,39 @@ export default class Viewer {
       this._oldMouseY = e.clientY;
       [this._oldMediaTop, this._oldMediaLeft] = this.mediaPosition;
     }
+
+    let i = 0;
+    let hoveredIdx = -1;
+    let area = Infinity;
+
+    for (; i < this._rects.length; ++i) {
+      let { x1, y1, x2, y2 } = this._rects[i];
+
+      if (x1 > x2) {
+        const xt = x2;
+        x2 = x1;
+        x1 = xt;
+      }
+
+      if (y1 > y2) {
+        const yt = y2;
+        y2 = y1;
+        y1 = yt;
+      }
+
+      if (x1 < e.clientX && e.clientX < x2
+        && y1 < e.clientY && e.clientY < y2
+        && area > (x2 - x1) * (y2 - y1)) {
+          hoveredIdx = i;
+          area = (x2 - x1) * (y2 - y1);
+      }
+
+      this._rects[i].hovered = false;
+    }
+
+    if (hoveredIdx != -1) {
+      this._rects[hoveredIdx].hovered = true;
+    }
   }
 
   /**
@@ -236,11 +359,64 @@ export default class Viewer {
   resizeHandler() {
     this.cacheViewerDimensions();
     this.resetMedia();
+    this.resizeCanvas();
+  }
+
+  /**
+   * Called when a video is loaded
+   */
+  videoLoadHandler() {
+    this._mediaHeight = this._video.videoHeight;
+    this._mediaWidth = this._video.videoWidth;
+    this._aspectRatio = this._mediaWidth / this._mediaHeight;
+    this.resetMedia();
   }
 
   /*****************************************************************************
   **                               GETTERS                                    **
   *****************************************************************************/
+
+  /**
+   * @returns The rects array for the current frame
+   */
+  get _rects() {
+    const frame = this.videoLoaded ? this._video.currentTime : 0;
+    const datum = this._data[frame] || {
+      rects: []
+    };
+    this._data[frame] = datum;
+    return datum.rects;
+  }
+
+  /**
+   * @returns Whether an image is loaded or not
+   */
+  get imageLoaded() {
+    return this._image.src !== "";
+  }
+
+  /**
+   * @returns The limits of the current media
+   */
+  get limits() {
+    const [top, left] = this.mediaPosition;
+    const [height, width] = this.mediaDimensions;
+    const topLimit = Math.max(0, top);
+    const leftLimit = Math.max(0, left);
+    const bottomLimit = Math.min(this._viewerHeight, top + height);
+    const rightLimit = Math.min(this._viewerWidth, top + width);
+    return { topLimit, bottomLimit, leftLimit, rightLimit };
+  }
+
+  /**
+   * @returns An array [top, left] representing the media's position
+   */
+  get mediaDimensions() {
+    return [
+      Number.parseInt(this._media.style.height.replace("px", "")),
+      Number.parseInt(this._media.style.width.replace("px", ""))
+    ];
+  }
 
   /**
    * @returns An array [top, left] representing the media's position
@@ -250,6 +426,13 @@ export default class Viewer {
       Number.parseInt(this._media.style.top.replace("px", "")),
       Number.parseInt(this._media.style.left.replace("px", ""))
     ];
+  }
+
+  /**
+   * @returns Whether a video is loaded or not
+   */
+  get videoLoaded() {
+    return this._video.src !== "";
   }
 
   /**
